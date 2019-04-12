137e3750b60e41ecdeada8a1629d9adc4fb64e08
Stephen <stephenbylo@gmail.com>
2019-04-12 14:24:30 +0800

    changed

--- go.sh
+++ go.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 
-clear; echo mutual_tls; read 
+clear; echo demo mutual_tls; read 
 cd mutual_tls ; ./go.sh; cd ..
 sleep 1
 
-clear; echo simple_routing; read
+clear; echo demo simple_routing; read
 cd simple_routing ; ./go.sh; cd ..
 sleep 1
 
-clear; echo fault-injection; read 
+clear; echo demo fault-injection; read 
 cd fault-injection ; ./go.sh cd ..
